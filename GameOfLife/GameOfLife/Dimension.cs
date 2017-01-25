7eb091b71e2ab70a98e26deaa63f2f5c103a3822
Phil Wright <pwright@foxguardsolutions.com>
2017-01-25 18:00:23 -0500

    Added Dimension class for Grid

---
+++ GameOfLife/GameOfLife/Dimension.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace GameOfLife
+{
+    public class Dimension
+    {
+        private bool _wraps;
+        public uint Min { get; private set; }
+        public uint Max { get; private set; }
+
+        public Dimension(uint size, bool wraps)
+        {
+            Min = 0;
+            Max = size - 1;
+            _wraps = wraps;
+        }
+
+        public IEnumerable<uint> GetNeighborValues(uint currentValue)
+        {
+            var isLowestValue = currentValue == Min;
+            var isHighestValue = currentValue == Max;
+            if (isLowestValue && isHighestValue)
+                return GetNeighborsOfOnlyValue();
+
+            if (isLowestValue)
+                return GetNeighborsOfLowestValue();
+
+            if (isHighestValue)
+                return GetNeighborsOfHighestValue();
+
+            return GetNeighborsOfNonEdgeValue(currentValue);
+        }
+
+        private IEnumerable<uint> GetNeighborsOfOnlyValue()
+        {
+            yield return Min;
+        }
+
+        private IEnumerable<uint> GetNeighborsOfLowestValue()
+        {
+            yield return Min;
+            yield return Min + 1;
+            if (_wraps)
+                yield return Max;
+        }
+
+        private IEnumerable<uint> GetNeighborsOfHighestValue()
+        {
+            yield return Max - 1;
+            yield return Max;
+            if (_wraps)
+                yield return Min;
+        }
+
+        private IEnumerable<uint> GetNeighborsOfNonEdgeValue(uint currentValue)
+        {
+            if (currentValue > Min && currentValue < Max)
+            {
+                yield return currentValue - 1;
+                yield return currentValue;
+                yield return currentValue + 1;
+            }
+        }
+
+        public bool IsOwnNeighbor()
+        {
+            return Max == 0 && _wraps;
+        }
+    }
+}
---
+++ GameOfLife/GameOfLifeTests/DimensionTests.cs
@@ -0,0 +1,124 @@
+﻿using GameOfLife;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace GameOfLifeTests
+{
+    public class DimensionTests : BaseTests
+    {
+        private bool _wraps;
+        private uint _size;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _wraps = Fixture.Create<bool>();
+            _size = Fixture.Create<uint>();
+        }
+
+        [Test]
+        public void IsOwnNeighbor_GivenDimensionSizeOfOne_ReturnsWrappingStatus()
+        {
+            var dimension = new Dimension(1, _wraps);
+
+            var isOwnNeighbor = dimension.IsOwnNeighbor();
+
+            Assert.That(isOwnNeighbor, Is.EqualTo(_wraps));
+        }
+
+        [Test]
+        public void IsOwnNeighbor_GivenDimensionSizeGreaterThanOne_ReturnsFalse()
+        {
+            var sizeGreaterThanOne = 1 + _size;
+            var dimension = new Dimension(sizeGreaterThanOne, _wraps);
+
+            var isOwnNeighbor = dimension.IsOwnNeighbor();
+
+            Assert.That(isOwnNeighbor, Is.False);
+        }
+
+        [Test]
+        public void GetNeighborValues_GivenValueGreaterThanMinAndLessThanMax_ReturnsValueAndValuesOnEitherSide()
+        {
+            var dimension = new Dimension(_size, _wraps);
+            var internalValue = Fixture.CreateInRange<uint>((int)dimension.Min + 1, (int)dimension.Max - 1);
+
+            var expectedNeighbors = new uint[] { internalValue - 1, internalValue, internalValue + 1 };
+
+            var neighbors = dimension.GetNeighborValues(internalValue);
+
+            Assert.That(neighbors, Is.EquivalentTo(expectedNeighbors));
+        }
+
+        [Test]
+        public void GetNeighborValuesOfMinValue_GivenWrapping_ReturnsMaxMinAndMinPlusOne()
+        {
+            var wraps = true;
+            var dimension = new Dimension(_size, wraps);
+            var expectedNeighbors = new uint[] { dimension.Min, dimension.Min + 1, dimension.Max };
+
+            var neighbors = dimension.GetNeighborValues(dimension.Min);
+
+            Assert.That(neighbors, Is.EquivalentTo(expectedNeighbors));
+        }
+
+        [Test]
+        public void GetNeighborValuesOfMinValue_GivenNoWrapping_ReturnsMinAndMinPlusOne()
+        {
+            var wraps = false;
+            var dimension = new Dimension(_size, wraps);
+            var expectedNeighbors = new uint[] { dimension.Min, dimension.Min + 1 };
+
+            var neighbors = dimension.GetNeighborValues(dimension.Min);
+
+            Assert.That(neighbors, Is.EquivalentTo(expectedNeighbors));
+        }
+
+        [Test]
+        public void GetNeighborValuesOfMaxValue_GivenWrapping_ReturnsMinMaxAndMaxMinusOne()
+        {
+            var wraps = true;
+            var dimension = new Dimension(_size, wraps);
+            var expectedNeighbors = new uint[] { dimension.Min, dimension.Max, dimension.Max - 1 };
+
+            var neighbors = dimension.GetNeighborValues(dimension.Max);
+
+            Assert.That(neighbors, Is.EquivalentTo(expectedNeighbors));
+        }
+
+        [Test]
+        public void GetNeighborValuesOfMaxValue_GivenNoWrapping_ReturnsMaxAndMaxMinusOne()
+        {
+            var wraps = false;
+            var dimension = new Dimension(_size, wraps);
+            var expectedNeighbors = new uint[] { dimension.Max, dimension.Max - 1 };
+
+            var neighbors = dimension.GetNeighborValues(dimension.Max);
+
+            Assert.That(neighbors, Is.EquivalentTo(expectedNeighbors));
+        }
+
+        [Test]
+        public void GetNeighborValues_GivenDimensionSizeOfOne_ReturnsOnlyValue()
+        {
+            var dimension = new Dimension(1, _wraps);
+            var expectedNeighbors = new uint[] { dimension.Min };
+
+            var neighbors = dimension.GetNeighborValues(dimension.Min);
+
+            Assert.That(neighbors, Is.EquivalentTo(expectedNeighbors));
+        }
+
+        [Test]
+        public void GetNeighborValues_GivenValueOutsideDimensionRange_ReturnsNoValues()
+        {
+            var dimension = new Dimension(_size, _wraps);
+            var valueOutsideRange = dimension.Max + Fixture.Create<uint>();
+            var expectedNeighbors = new uint[0];
+
+            var neighbors = dimension.GetNeighborValues(valueOutsideRange);
+
+            Assert.That(neighbors, Is.EquivalentTo(expectedNeighbors));
+        }
+    }
+}
