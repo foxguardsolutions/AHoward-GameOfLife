7c3baa0965e3f0516841cdc86e35f1c2dc480116
Taylor Bittner <tbittner@foxguardsolutions.com>
2017-06-09 15:08:36 -0400

    Game progresses to next generation
    A Game is given a starting generation number, a Grid, and a wrapping
    rule to determine how to define cell environments. When Proceed is
    called, it analyzes each cell environment, applies the appropriate
    Command to each cell, and increments the Generation number.

---
+++ GameOfLife/Game.cs
@@ -0,0 +1,40 @@
+﻿using GameOfLife.Environments;
+using GameOfLife.Operations;
+using System.Collections.Generic;
+using System.Linq;
+using GameOfLife.EnvironmentRules;
+
+namespace GameOfLife
+{
+    public class Game
+    {
+        private readonly IEnvironmentRules _rules;
+        private readonly List<CellEnvironment> _environments;
+        
+        public int Generation { get; private set; }
+        public Grid Grid { get; }
+
+        public Game(int generation, Grid grid, IEnvironmentRules rules, CellEnvironmentFactory factory)
+        {
+            Generation = generation;
+            Grid = grid;
+            _rules = rules;
+            _environments = factory.GetEnvironments(Grid).ToList();
+        }
+
+        public void Proceed()
+        {
+            var operations = _environments.Select(e => _rules.GetOperationToPerformOnCell(e)).ToList();
+
+            for (var i = 0; i < _environments.Count; i++)
+            {
+                var command = CommandFactory.GetCommand(operations[i]);
+                command.Execute(_environments[i].Cell);
+            }
+
+            Generation++;
+        }
+
+        public override string ToString() => $"Generation {Generation}:{GridFactory.NewLine}{Grid}";
+    }
+}
